be47ede82c3f2bdf7b014126afa6c5abb340ae82
Ahmad <nihoda3918@bultoc.com>
2026-05-02 04:27:17 +0300

    Fix GitHub Pages paths

--- TopFood_Package/TopFood/vite.config.ts
+++ TopFood_Package/TopFood/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Top-Food/',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000,
